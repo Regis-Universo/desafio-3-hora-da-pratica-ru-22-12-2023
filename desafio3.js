fda9ab56ba2b83228ed4cf6e3c008e7990fc7b22
Regis <regisuniverso@gmail.com>
2023-12-22 12:03:26 -0300

    modificando os arquivos

---
+++ desafio3.js
@@ -0,0 +1,44 @@
+// 1.  Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+
+// let contador = 1;
+// while (contador <= 10) {
+//     console.log(contador);
+//     contador ++;
+// }
+
+
+// 2. Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+// let contador2 = 10;
+// while (contador2 >= 0) {
+//     console.log(contador2);
+//     contador2 --;
+// }
+
+
+// 3. Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+
+// let contador3 = prompt('Digite um número de 1 a 15');
+
+// while (contador3 >= 0) {
+//     console.log(contador3);
+//     contador3 --;
+// }
+
+
+// 4. Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+
+let numero = prompt('Digite um número de 1 a 15');
+
+// Converte o input para um número inteiro
+numero = parseInt(numero);
+
+let contador4 = 0;
+
+while (contador4 <= numero) {
+    console.log(contador4);
+    contador4 ++;
+}
+
+
+
